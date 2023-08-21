1be1182b78386b36f9d1de6166e4e93f7f46d43a
unknown <mywarhio@gmail.com>
2023-08-21 15:09:40 +0300

    height tracker

--- Assets/Scripts/Player/PlayerHeightTracker.cs
+++ Assets/Scripts/Player/PlayerHeightTracker.cs
@@ -1,8 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerHeightTracker : MonoBehaviour
 {
-    public float height;
+    public float heightMeterRatio;
+    private void Update()
+    {
+        GameValues.height = transform.position.y * heightMeterRatio; 
+    }
 }
