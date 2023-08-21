a8793c10e850fba347dc3b0c7a00683e89ebe713
Xenoant <jeannot.lukatsch@gmail.com>
2023-08-21 12:02:06 +0200

    Add height

--- Assets/Scripts/Player/PlayerInputManager.cs
+++ Assets/Scripts/Player/PlayerInputManager.cs
@@ -1,18 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerInputManager : MonoBehaviour
+public class KeyInputScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // recognize: E (Interact), Q (Upgrade Campfire), ESC (close Menus, open ESC Menu), 1 (place Fire), 2 (place Barricade)
+    public PlayerInputActions playerInput;
+
+    private void OnEnable()
     {
-        
+        playerInput.Player.Enable();
+    }
+    private void OnDisable()
+    {
+        playerInput.Player.Disable();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        playerInput = new PlayerInputActions();
+    }
+
+
+    private void Start()
+    {
+        //playerInput.Player.interact.performed += Interact_performed;
+        playerInput.Player.Shoot.started += Shoot;
+        playerInput.Player.Shoot.canceled += Shoot;
+    }
+
+    private void Shoot(InputAction.CallbackContext ctx)
+    {
+        if (ctx.started)
+        {
+            // prepare launching
+            Debug.Log("Prepare...");
+        }
+        else if (ctx.canceled)
+        {
+            // shoot tentacle
+            Debug.Log("Launch...");
+            GetComponent<PlayerMovement>().Shoot();
+        }
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,8 +1,76 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    [Header("Tentacle Things")]
+    public GameObject[] tentacles;
+
+    private PlayerInputActions playerInput;
+    private InputAction mousePos;
+
+    private void OnDrawGizmos()
+    {
+        foreach (var t in tentacles)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(t.transform.position, 0.2f);
+        }
+    }
+
+    private void OnEnable()
+    {
+        playerInput.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Player.Disable();
+    }
+
+    private void Awake()
+    {
+        playerInput = new PlayerInputActions();
+        mousePos = playerInput.Player.MousePos;
+    }
+
+    private void Update()
+    {
+        int nearestTentacleIndex = GetNearestTentacle();
+        //Debug.Log("Nearest Tentacle Index: " + nearestTentacleIndex);
+    }
+
+    public void Shoot()
+    {
+
+    }
+
+    public int GetNearestTentacle()
+    {
+        int shortestIndex = 0;
+        float shortestDist = Mathf.Infinity;
+
+        Vector2 mouseScreenPosition = mousePos.ReadValue<Vector2>();
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3(mouseScreenPosition.x, mouseScreenPosition.y, Camera.main.nearClipPlane));
+        mouseWorldPosition.z = 0;
+
+        //Debug.Log(mouseWorldPosition);
+
+        for (int i = 0; i < tentacles.Length; i++)
+        {
+            GameObject tentacle = tentacles[i];
+            float dist = Vector3.Distance(tentacle.transform.position, mouseWorldPosition);
+
+            //Debug.Log("Dist: " + dist);
+
+            if (dist < shortestDist)
+            {
+                //Debug.Log("Dist: " + dist);
+                shortestIndex = i;
+                shortestDist = dist;
+            }
+        }
+
+        return shortestIndex;
+    }
 }
