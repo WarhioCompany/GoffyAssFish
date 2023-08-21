982fa0a2d6347e1fb6a164d29a36b89ebb1e84aa
Elyes <126920308+ETechyDev@users.noreply.github.com>
2023-08-21 21:08:58 +0100

    update

--- Assets/Scripts/Systemic/SpawnManager.cs
+++ Assets/Scripts/Systemic/SpawnManager.cs
@@ -13,48 +13,51 @@ public class SpawnManager : MonoBehaviour
     private float cooldownTimer = 0.0f;
     private float RotationRange = 180f;
 
-    private float GetRandomDelay(float minDelayTime, float maxDelayTime) { 
+    private float GetRandomDelay(float minDelayTime, float maxDelayTime)
+    {
         return Random.Range(minDelayTime, maxDelayTime);
     }
 
-    private GameObject GetRandomPrefab(GameObject[] prefabs) {
+    private GameObject GetRandomPrefab(GameObject[] prefabs)
+    {
         int randomIndex = Random.Range(0, prefabs.Length);
         return prefabs[randomIndex];
     }
 
-    private float GetRandomXPos(float xRange) {
+    private float GetRandomXPos(float xRange)
+    {
         return Random.Range(-xRange, xRange);
     }
 
-    private Vector3 GetRandomRotation(float range) { 
-        float x = Random.Range(-range, range);
-        float y = Random.Range(-range, range);
-        float z = Random.Range(-range, range);
-        return new Vector3(x, y, z);
+    private float GetRandomZRotation(float range)
+    {
+        return Random.Range(-range, range);
     }
 
     private void Spawn()
     {
         GameObject prefab = GetRandomPrefab(Objects);
-        if(prefab != null)
+        if (prefab != null)
         {
             GameObject _object = Instantiate(prefab);
-            SpawnedObject spawned =_object.AddComponent<SpawnedObject>();
+            SpawnedObject spawned = _object.AddComponent<SpawnedObject>();
             spawned.LimitlineTag = LimitlineTag;
             _object.transform.position = new Vector3(GetRandomXPos(XRange), Camera.main.transform.position.y);
-            _object.transform.eulerAngles = GetRandomRotation(RotationRange);
+            _object.transform.eulerAngles += new Vector3(0, 0, GetRandomZRotation(RotationRange));
         }
     }
 
     private void TrySpawn()
     {
-        if(cooldownTimer < Time.time) {
+        if (cooldownTimer < Time.time)
+        {
             Spawn();
             cooldownTimer = Time.time + GetRandomDelay(SpawnMinTime, SpawnMaxTime);
         }
     }
 
-    private void Update(){
+    private void Update()
+    {
         TrySpawn();
     }
 }
