fdab14d243e7651737c6b48eee40a1b73051d3bf
Aizen Sosuke <Aizen Sosuke@DESKTOP-M600QU1>
2023-08-22 12:47:57 +0100

    Revert "Revert "Bubbles""
    This reverts commit 4d6c7a14c5eef1739f8462eafbfa5eca99699099.

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    // Player's level
+    public int Level { get; set; }
+
+    // Player's experience
+    public float ExperiencePoints { get; set; }
+
+    // Static Constructor
+    public static PlayerStats Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+}
