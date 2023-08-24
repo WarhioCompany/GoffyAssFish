7f730e0cdd6dea771fd6769827ce42930a9bb15f
Elyes <126920308+ETechyDev@users.noreply.github.com>
2023-08-24 14:51:11 +0100

    UI Main Menu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,27 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    // ~ Buttons
+    [Header("Buttons")]
+    [SerializeField] private Button StartButton;
+    [SerializeField] private Button OptionsButton;
+    [SerializeField] private Button CreditsButton;
+    [SerializeField] private Button ExitButton;
+
+    // ~ Scene Loader
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+
+    // ~ For Exit Button (UnityEvent)
+
+    public void ExitGame() {
+        EditorApplication.ExitPlaymode();
+    }
+}
