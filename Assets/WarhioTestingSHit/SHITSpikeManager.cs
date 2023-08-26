6a4a6db6b3440f45094948f0870dfed9b78193b6
Xenoant <jeannot.lukatsch@gmail.com>
2023-08-26 23:20:31 +0200

    push and such

--- Assets/WarhioTestingSHit/SHITSpikeManager.cs
+++ Assets/WarhioTestingSHit/SHITSpikeManager.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
-using static UnityEditor.PlayerSettings;
 
 public class SHITSpikeManager : MonoBehaviour
 {
