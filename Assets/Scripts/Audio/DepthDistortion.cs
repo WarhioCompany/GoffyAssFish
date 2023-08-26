9128a3ed53c7d3a607eb9d2be3ea5368631d33bb
Xenoant <jeannot.lukatsch@gmail.com>
2023-08-27 01:30:14 +0200

    Music distortion works now

--- Assets/Scripts/Audio/DepthDistortion.cs
+++ Assets/Scripts/Audio/DepthDistortion.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 public class DepthDistortion : MonoBehaviour
 {
@@ -25,7 +26,18 @@ public class DepthDistortion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mixer.SetFloat("depthEffect", maxEffectValue - (maxEffectValue - minEffectValue) / GameValues.MaxHeight * -GameValues.height); 
+        // berechne prozent von aktueller höhe zu maxhöhe
+
+        // value = prozent * maxvalue + minvalue
+
+        if (SceneManager.GetActiveScene().name == "Main")
+        {
+            float percent = (-GameValues.height * GameValues.heightMeterRatio) / GameValues.MaxHeight;
+            float value = (1-percent*2) * maxEffectValue + minEffectValue;
+            mixer.SetFloat("depthEffect", value);
+        }
+
+        //mixer.SetFloat("depthEffect", maxEffectValue - (maxEffectValue - minEffectValue) / GameValues.MaxHeight * -GameValues.height); 
         mixer.SetFloat("depthVolumeBoost", maxBoostValue / GameValues.MaxHeight * GameValues.height);
     }
 }
